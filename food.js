fc6ea05551ab5153a73e7a97e98f53b598e803e6
J-Sargent <blythetruth@gmail.com>
2019-05-25 16:05:38 -0500

    class Food,Fruit,Local, NotLocal, Input fields working, now make new array.

--- food.js
+++ food.js
@@ -1,25 +1,46 @@
 //console.log(JSON.stringify(obj))
 console.log("Hello Foodie!");
+var orange = ["fruit", "orange", ".33", "Florida", "orange"];
+var apple = ["fruit", "apple", ".50", "Kansas", "Red", "organic"];
+var newFruitArray = [];
+
+function makeNewFruitArray() {
+  console.log("makeNewFruitArray called");
+  newFruitArray.push(document.getElementById("categoryInput").value);
+  newFruitArray.push(document.getElementById("typeInput").value);
+  newFruitArray.push(document.getElementById("priceInput").value);
+  newFruitArray.push(document.getElementById("locationInput").value);
+  newFruitArray.push(document.getElementById("colorInput").value);
+  newFruitArray.push(document.getElementById("growingMethodInput").value);
+  newFruitArray.push(document.getElementById("distanceInput").value);
+  console.log(newFruitArray);
+}
 
 class Food {
-  constructor(type, price, location, color) {
+  constructor(category, type, price) {
+    this.category = category;
     this.type = type;
     this.price = price;
-    this.location = location;
-    this.color = color;
   }
-  displayType() {
-    console.log("The type is " + this.type);
+  displayCategory() {
+    console.log("This " + this.type + " is a " + this.category);
   }
 }
 
-class Local extends Food {
-  constructor(type, price, location, color, growingMethod) {
-    super(type, price, location, color);
-    //    this.type = type;
-    //    this.price = price;
-    //    this.location = location;
-    //    this.color = color;
+class Fruit extends Food {
+  constructor(category, type, price, color, location) {
+    super(category, type, price);
+    this.color = color;
+    this.location = location;
+  }
+  displayFruit() {
+    console.log("This is a fruit");
+  }
+}
+
+class Local extends Fruit {
+  constructor(category, type, price, color, location, growingMethod) {
+    super(category, type, price, color, location);
     this.growingMethod = growingMethod;
   }
   displayLocal() {
@@ -30,9 +51,25 @@ class Local extends Food {
   }
 }
 
-let orange = new Food("orange", ".33", "Florida", "orange");
-orange.displayType();
+class NotLocal extends Fruit {
+  constructor(category, type, price, color, location, growingMethod, distance) {
+    super(category, type, price, color, location);
+    this.distance = distance;
+  }
+  displayDistance() {
+    console.log("This food traveled " + this.distance + " many miles.");
+  }
+}
 
-let apple = new Local("apple", ".50", "Kansas", "Red", "organic");
-apple.displayLocal();
-apple.displayGrowingType();
+function sortLocal() {
+  console.log("sortLocal called");
+  if (apple.includes("Kansas")) {
+    console.log("Kansas");
+    let apple = new Local("fruit", "apple", ".50", "Kansas", "Red", "organic");
+    apple.displayCategory();
+    apple.displayFruit();
+    apple.displayGrowingType();
+  }
+}
+
+sortLocal();
